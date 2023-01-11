cb5d031f86a849f356b4af3a54a6c931a7c53334
Jesushs4 <lagt5799@gmail.com>
2023-01-11 19:19:34 +0100

    Ej1 Ej6

--- Ejercicio1/Caballo.java
+++ Ejercicio1/Caballo.java
@@ -1,3 +1,48 @@
 public class Caballo {
     
+    private String nombre, color, raza;
+    private int edad, velocidad;
+
+    Caballo (String n, String c, String r, int e, int v) {
+        this.nombre = n;
+        this.color = c;
+        this.raza = r;
+        this.edad = e;
+        this.velocidad = v;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public String getRaza() {
+        return this.raza;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public int getVelocidad() {
+        return this.velocidad;
+    }
+
+    public void relincha() {
+        System.out.print("Hiieeeeeeeeeeee");
+    }
+
+    public void come() {
+        System.out.print("Ñam ñam ñam");
+    }
+
+    @Override
+    public String toString() {
+        return "El nombre del caballo es "+nombre+", su color es "+color+", pertenece a la raza "+raza+", tiene "+edad+" años y corre a "+velocidad+"m/s";
+    }
+
+
 }
--- Ejercicio1/Ejercicio1.java
+++ Ejercicio1/Ejercicio1.java
@@ -1,5 +1,16 @@
-package Ejercicio1;
-
 public class Ejercicio1 {
+    public static void main (String args[]) {
+        Caballo a = new Caballo("Marco", "marrón", "Morgan", 10, 60);
+        Caballo b = new Caballo("Pepito", "blanco", "andaluz", 14, 70);
+
+        System.out.println(a);
+        System.out.println(b);
+
+        a.relincha();
+        System.out.println();
+        b.come();
+
+
+    }
     
 }
